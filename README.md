7e6d25c1cda934bbbef43b9144666dc47d9aaf31
Rye Terrell <ryeterrell@ryeterrell.net>
2016-01-01 16:46:10 -0600

    refactor for cubemaps

--- README.md
+++ README.md
@@ -10,28 +10,11 @@ npm install gl-cubemap-placeholder
 
 ## API
 ```js
-var debugCubemap = require('gl-cubemap-placeholder');
+var createCubemap = require('gl-cubemap-placeholder');
 ```
 
-#### `var textures = debugCubemap(gl)`
+#### `var cubemap = createCubemap(gl, size)`
 
-Takes a WebGL context, returns a collection of disjoint cubemap textures
-in the form:
-
-```js
-{
-    pos: {
-        x: gl-texture2d texture,
-        y: gl-texture2d texture,
-        z: gl-texture2d texture
-    },
-    neg: {
-        x: gl-texture2d texture,
-        y: gl-texture2d texture,
-        z: gl-texture2d texture
-    }
-}
-```
-
-See [gl-texture2d](https://github.com/stackgl/gl-texture2d) for more
-information about the returned texture objects.
+Takes a WebGL context, returns a
+[gl-texture-cube](https://github.com/wwwtyro/gl-texture-cube)
+with resolution `size`.
--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-'use strict';
+'use strict'
 
-var Texture2D = require('gl-texture2d');
+var createTextureCube = require('gl-texture-cube')
 
-var signs = ['pos', 'neg'];
-var axes = ['x', 'y', 'z'];
+var signs = ['pos', 'neg']
+var axes = ['x', 'y', 'z']
 
-module.exports = function genTextures(gl) {
-    var textures = {
-        pos: {},
-        neg: {}
-    };
-    var size = 512;
-    for (var i = 0; i < signs.length; i++) {
-        var sign = signs[i];
-        for (var j = 0; j < axes.length; j++) {
-            var axis = axes[j];
-            var canvasT = document.createElement('canvas');
-            canvasT.width = canvasT.height = size;
-            var ctx = canvasT.getContext('2d');
-            ctx.fillStyle = 'rgb(0,64,128)';
-            ctx.fillRect(0, 0, size, size);
-            ctx.fillStyle = 'white';
-            ctx.fillRect(8, 8, size-16, size-16);
-            ctx.fillStyle = 'rgb(0,64,128)';
-            ctx.font = size/4 + 'px serif';
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            ctx.fillText(sign+'-'+axis, size/2, size/2);
-            ctx.strokeStyle = 'rgb(0,64,128)';
-            ctx.strokeRect(0, 0, size, size);
-            textures[sign][axis] = Texture2D(gl, canvasT);
-            textures[sign][axis].magFilter = gl.LINEAR;
-        }
+module.exports = function genTextures (gl, size) {
+  var textures = {
+    pos: {},
+    neg: {}
+  }
+  for (var i = 0; i < signs.length; i++) {
+    var sign = signs[i]
+    for (var j = 0; j < axes.length; j++) {
+      var axis = axes[j]
+      if (axis === 'x' || axis === 'z') {
+        ctx.translate(size, size)
+        ctx.rotate(Math.PI)
+      }
+      var canvas = document.createElement('canvas')
+      canvas.width = canvas.height = size
+      var ctx = canvas.getContext('2d')
+      ctx.fillStyle = 'rgb(0,64,128)'
+      ctx.fillRect(0, 0, size, size)
+      ctx.fillStyle = 'white'
+      ctx.fillRect(8, 8, size - 16, size - 16)
+      ctx.fillStyle = 'rgb(0,64,128)'
+      ctx.font = size / 4 + 'px serif'
+      ctx.textAlign = 'center'
+      ctx.textBaseline = 'middle'
+      ctx.fillText(sign + '-' + axis, size / 2, size / 2)
+      ctx.strokeStyle = 'rgb(0,64,128)'
+      ctx.strokeRect(0, 0, size, size)
+      textures[sign][axis] = createTextureCube(gl, canvas)
     }
-    return textures;
+  }
+  return textures
 }
